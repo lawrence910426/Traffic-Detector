52187cbe9868848052be986f28c5791746114c3e
lawrence <lawrence910426@gmail.com>
2022-10-02 17:42:31 +0800

    fix: progress percent

--- video-detector/deepsort-counter.py
+++ video-detector/deepsort-counter.py
@@ -180,7 +180,7 @@ class Sharingan(object):
             # logging
             log = "time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}, " \
                 .format(end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs))
-            log += progress.get_progress(idx_frame / len(fixed_transform))
+            log += progress.get_progress(idx_frame / len(fixed_transform) * 100)
             self.logger.info(log)
 
         flow = {}
