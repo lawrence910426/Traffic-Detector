93107ebfc59132e0df5a709577ef23d8799b1c70
lawrence <lawrence910426@gmail.com>
2022-11-01 15:14:03 +0800

    fix: remove dill

--- video-uploader/wrapper.py
+++ video-uploader/wrapper.py
@@ -6,30 +6,17 @@ import pickle
 import argparse
 import dill
 
-parser = argparse.ArgumentParser("Youtube wrapper")
-parser.add_argument(
-    "--step", 
-    help="Either `url` for retrieve url or `auth` for apply authentication code", 
-)
-args = parser.parse_args()
+child = pexpect.spawn('python3 youtube.py')
+# child.logfile = sys.stdout.buffer
 
-if args.step == 'url':
-    child = pexpect.spawn('python3 youtube.py')
-    # child.logfile = sys.stdout.buffer
+child.expect_exact("Please visit this URL to authorize this application: ")
+line = child.readline(1)
+print(line)
 
-    child.expect_exact("Please visit this URL to authorize this application: ")
-    line = child.readline(1)
-    print(line)
+child.expect_exact("Enter the authorization code: ")
+out = child.sendline (input())
 
-    handle = dill.dumps(child)
-
-if args.step == 'auth':
-    child = dill.loads(handle)
-
-    child.expect_exact("Enter the authorization code: ")
-    out = child.sendline (input())
-
-    child.expect_exact("Youtube link: ")
-    line = child.readline(1)
-    print(line)
+child.expect_exact("Youtube link: ")
+line = child.readline(1)
+print(line)
 
