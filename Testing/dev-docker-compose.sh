434aae49cfbe7042992c7f70ee9dde68c4f9d0f7
lawrence <lawrence910426@gmail.com>
2022-11-28 21:41:19 +0800

    feat: script & Testing

---
+++ Testing/dev-docker-compose.sh
@@ -0,0 +1,7 @@
+export BACKEND_HOST=http://203.145.214.46/api/
+export LOCAL_IP=192.168.211.4
+
+cd ..
+# docker-compose down
+# docker-compose build --no-cache
+docker-compose up
---
+++ backend/scripts/upload_youtube_video.sh
@@ -0,0 +1,1 @@
+module load miniconda3 && conda activate youtube-upload && cd /work/lawrence0426/Sharingan/video-uploader/ && python youtube.py --file {videoPath}
