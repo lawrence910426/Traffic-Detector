d0e77ee7b97cfdb0f235fb9b6a5770aef68f4372
lawrence <lawrence910426@gmail.com>
2022-12-06 00:28:45 +0800

    fix: long videos

--- backend/scripts/init_task.sh
+++ backend/scripts/init_task.sh
@@ -1,1 +1,1 @@
-echo "nohup conda run -n detector python deepsort-counter.py videos/{video_id} --detector_line {detector} --output_name {output_id} --stable_period {stabilization} &> /dev/null & ln -s ../output/{output_id}.out scripts/slurm-{uuid}.out & exit" | nc {LOCAL_IP} 8787
+echo "nohup conda run -n detector python deepsort-counter.py videos/{video_id} --detector_line {detector} --output_name {output_id} --stable_period {stabilization} &> nohup.out & ln -s ../output/{output_id}.out scripts/slurm-{uuid}.out & exit" | nc {LOCAL_IP} 8787
--- video-detector/utils/draw.py
+++ video-detector/utils/draw.py
@@ -40,7 +40,7 @@ def draw_flow(img, flow):
         t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 ,2)[0]
         cv2.putText(
             img, label, (10, 10 + (t_size[1] + 3) * (i + 1)), 
-            cv2.FONT_HERSHEY_PLAIN, 2, [0, 0, 0], 4
+            cv2.FONT_HERSHEY_PLAIN, 2, [0, 0, 0], 5
         )
         cv2.putText(
             img, label, (10, 10 + (t_size[1] + 3) * (i + 1)), 
