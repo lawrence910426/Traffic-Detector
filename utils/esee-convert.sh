dc692342eb5469573e08bf25346dfe42063ba7e7
lawrence <lawrence910426@gmail.com>
2023-06-14 14:22:14 +0800

    feat: esee-convert.sh

--- utils/esee-convert.sh
+++ utils/esee-convert.sh
@@ -6,22 +6,24 @@ then
 	echo "e.g. ./esee-convert mp4"
 	exit
 fi
-
+original_pos=$(pwd)
 output_file=out.$1
-temp_pos=/tmp/video
+temp_pos=~/.tmp/video
+
+mkdir ~/.tmp
+mkdir ~/.tmp/video
+
 # Move all videos to temp
 rm -r $temp_pos
 mkdir $temp_pos
 
 # Aggregate files for at most 3 layers 
-mv *.TS $temp_pos
-mv */*.TS $temp_pos
-mv */*/*.TS $temp_pos
+cp *.TS $temp_pos
+cp */*.TS $temp_pos
+cp */*/*.TS $temp_pos
 
 # Generate video
 cd $temp_pos
 ls -1 * | awk '/\.TS/{print "file " $0}' > filelist.txt
-ffmpeg -f concat -safe 0 -i filelist.txt -c copy output.mkv
-mv output_file $PWD
-
-
+ffmpeg -f concat -safe 0 -i filelist.txt -c copy $output_file
+cp $temp_pos/$output_file "$original_pos"
