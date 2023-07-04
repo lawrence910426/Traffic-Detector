940c7400d6d70536ffd8fd36ffb9ee1afe369b3b
lawrence910426 <lawrence910426@gmail.com>
2023-07-04 13:00:07 +0800

    fix: nextcloud integrate

--- frontend/src/1-Upload/Upload.js
+++ frontend/src/1-Upload/Upload.js
@@ -17,88 +17,41 @@ class Upload extends React.Component {
     this.state = { progress: 0 };
   }
 
-  async droppedFiles(files) {
-    var fileObj = files[0]
-    console.log(fileObj)
+  change(event) {
+    this.props.video(event.target.value);
+  }
 
-    var vid = await this.uploadFileServer(fileObj);
-    this.props.video(vid)
-
+  complete() {
     this.props.next();
-  }
-
-  async uploadFileServer(file) { 
-      let formData = new FormData(); 
-      formData.append("file", file);
-
-      let resp = await axios.request({
-          method: "post", 
-          url: config.host + "upload", 
-          data: formData, 
-          onUploadProgress: (p) => {
-            console.log(p); 
-            this.setState({
-              progress: p.loaded / p.total * 100
-            })
-          }
-      })
-
-      this.setState({
-        progress: 100,
-      })
-
-      return resp.data.id
-  }
-
-  renderProgressBar() {
-    if (this.state.progress != 0) {
-      return (
-        <Row style={{ marginTop: '1rem' }}>
-          <Col xs={6} md={2}><label>影片上傳進度：</label></Col>
-          <Col>
-            <div className="progress" style={{height: '20px'}}>
-              <div className="progress-bar" 
-                  role="progressbar" 
-                  style={{width: this.state.progress + '%'}} 
-                  aria-valuenow={this.state.progress} 
-                  aria-valuemin="0" aria-valuemax="100">
-                  {Math.floor(this.state.progress)}%
-              </div>
-            </div>
-          </Col>
-        </Row>
-      )
-    }
   }
 
   render() {
     return (
       <div>
-        <Dropzone onDrop={this.droppedFiles.bind(this)}>
-          {({getRootProps, getInputProps}) => (
-            <section>
-              <div {...getRootProps()} style={{
-                borderStyle: 'dashed',
-                height: '20rem'
-              }}>
-                <input {...getInputProps()} />
-                <p style={{
-                  textAlign: 'center',
-                  marginTop: '3rem'
-                }}>將檔案拖曳到此以上傳影片</p>
-
-                <MDBIcon fas icon="cloud-upload-alt" style={{
-                  textAlign: 'center',
-                  width: '100%',
-                  fontSize: '10rem',
-                  marginTop: '1rem'
-                }} />
-              </div>
-            </section>
-          )}
-        </Dropzone>
-
-        { this.renderProgressBar() }
+        <Container>
+          <Row><Col>
+            <MDBCard>
+              <MDBCardBody>
+                <MDBCardTitle>輸入影片路徑</MDBCardTitle>
+                <Container>
+                  <Row>
+                    <Col>
+                      <MDBBtn onClick={window.open(config.nextcloud)}>開啟 NextCloud</MDBBtn>
+                    </Col>
+                    <Col>
+                      <MDBInput label='Video Path' id='path' type='text' onChange={this.change.bind(this)} />
+                    </Col>
+                  </Row>
+                </Container>
+              </MDBCardBody>
+            </MDBCard>
+            <input name="filePath"></input>
+          </Col></Row>
+          
+          <Row style={{ marginTop: '3rem' }}><Col>
+            <MDBBtn onClick={this.complete.bind(this)}>下一步</MDBBtn>
+          </Col></Row>
+        </Container>
       </div>
     );
   }
